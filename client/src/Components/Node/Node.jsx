eed9aa96280de15d6183ca1fd90da3c38ccdd13a
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-08-17 19:57:34 -0700

    created node on main page

--- client/src/Components/Node/Node.jsx
+++ client/src/Components/Node/Node.jsx
@@ -1,24 +1,67 @@
-import React, { Fragment } from "react";
-import ReactFlow from "react-flow-renderer";
+import { useCallback, useState } from "react";
+import ReactFlow, {
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+} from "react-flow-renderer";
+
+import TextUpdaterNode from "./TextUpdaterNode";
+
+import "./TextUpdaterNode.scss";
+
+const rfStyle = {
+    backgroundColor: "#B8CEFF",
+};
 
 const initialNodes = [
     {
-        id: "1",
-        type: "input",
-        data: { label: "Node" },
+        id: "node-1",
+        type: "textUpdater",
         position: { x: 0, y: 0 },
+        data: { value: 123 },
+    },
+    {
+        id: "node-2",
+        type: "textUpdater",
+        position: { x: 300, y: -30 },
+        data: { value: 1234 },
     },
 ];
+// we define the nodeTypes outside of the component to prevent re-renderings
+// you could also use useMemo inside the component
+const nodeTypes = { textUpdater: TextUpdaterNode };
 
-const Node = () => {
-    return (
-        <Fragment>
-            <ReactFlow
-                nodes={initialNodes}
-                style={{ width: "100%", height: "90vh" }}
-            />
-        </Fragment>
+function Node() {
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState([]);
+
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes]
     );
-};
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges]
+    );
+    const onConnect = useCallback(
+        (connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges]
+    );
+
+    return (
+        <div style={{ height: 800 }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                nodeTypes={nodeTypes}
+                fitView
+                style={rfStyle}
+            />
+        </div>
+    );
+}
 
 export default Node;
---
+++ client/src/Components/Node/TextUpdaterNode.jsx
@@ -0,0 +1,33 @@
+import { useCallback } from "react";
+import { Handle, Position } from "react-flow-renderer";
+
+const handleStyle = { left: 10 };
+
+function TextUpdaterNode({ data }) {
+    const onChange = useCallback((evt) => {
+        console.log(evt.target.value);
+    }, []);
+
+    return (
+        <div className="text-updater-node">
+            <Handle type="target" position={Position.Top} />
+            <div>
+                <input
+                    id="text"
+                    name="text"
+                    onChange={onChange}
+                    className="input"
+                />
+            </div>
+            <Handle
+                type="source"
+                position={Position.Bottom}
+                id="a"
+                style={handleStyle}
+            />
+            <Handle type="source" position={Position.Right} id="b" />
+        </div>
+    );
+}
+
+export default TextUpdaterNode;
