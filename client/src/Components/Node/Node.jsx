375223fdace6f76d958b42896871ddb2fc948753
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-08-23 17:58:54 -0700

    created grand child

--- client/src/Components/Node/Node.jsx
+++ client/src/Components/Node/Node.jsx
@@ -5,6 +5,7 @@ import ChildNode from "./ChildNode";
 import "./Node.scss";
 
 const childrenData = ["child-1", "child-2", "child-3"];
+const grandChildrenData = ["grandChild-1", "grandChild-2", "grandChild-3"];
 
 const nodeTypes = { parentNode: ParentNode, childNode: ChildNode };
 
@@ -38,6 +39,28 @@ const initialNodes = [
         type: "childNode",
         position: { x: 250, y: 200 },
     },
+    // Grand Child Node
+    {
+        id: grandChildrenData[0],
+        sourcePosition: "right",
+        targetPosition: "left",
+        type: "childNode",
+        position: { x: 500, y: -270 },
+    },
+    {
+        id: grandChildrenData[1],
+        sourcePosition: "right",
+        targetPosition: "left",
+        type: "childNode",
+        position: { x: 500, y: -200 },
+    },
+    {
+        id: grandChildrenData[2],
+        sourcePosition: "right",
+        targetPosition: "left",
+        type: "childNode",
+        position: { x: 500, y: -130 },
+    },
 ];
 
 const initialEdges = [
@@ -57,6 +80,25 @@ const initialEdges = [
         id: "parent-child3",
         source: "parent",
         target: "child-3",
+        animated: true,
+    },
+
+    {
+        id: "child1-grandChild1",
+        source: "child-1",
+        target: "grandChild-1",
+        animated: true,
+    },
+    {
+        id: "child1-grandChild2",
+        source: "child-1",
+        target: "grandChild-2",
+        animated: true,
+    },
+    {
+        id: "child1-grandChild3",
+        source: "child-1",
+        target: "grandChild-3",
         animated: true,
     },
 ];
