2887618821b0e55814cc81180791afe527b5c686
Eisuke Esaki <acekaydev@gmail.com>
2022-08-29 15:40:09 -0700

    deleted client application from repo

--- client/src/Components/CreateMap/Content.js
+++
@@ -1,9 +0,0 @@
-import css from './content.module.css';
-
-function Content(props) {
-    return (<div className={css.container}>
-        {props.component}
-    </div>);
-}
-
-export default Content;
--- client/src/Components/CreateMap/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home";
-import Map from "./Map";
-
-const routes = {
-    'home': { component: <Home />, breadcrumbs: ['home'] },
-    'map': { component: <Map />, breadcrumbs: ['home', 'map'] },
-};
-
-export default routes;
